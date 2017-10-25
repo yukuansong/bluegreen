c0fa933e791b3a7267605e16667d81ea8345cdfe
yukuansong <yukuan.song@gmail.com>
2017-10-24 22:30:22 -0400

    Update image.php
    testing the web hook trigger

--- image.php
+++ image.php
@@ -4,7 +4,7 @@ $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 
 $deployment = getenv("COLOR");
-
+// testing webhook triger
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
